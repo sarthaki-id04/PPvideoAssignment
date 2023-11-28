02aaa24c64264059f5c8fe106c7deec4054a4561
sarthaki-id04 <149144156+sarthaki-id04@users.noreply.github.com>
2023-11-28 21:28:26 +0530

    Create Defcons.cpp

---
+++ Programs/Defcons.cpp
@@ -0,0 +1,33 @@
+Program:
+
+#include <iostream> using namespace std;
+
+class stu
+
+{
+
+Private:
+
+int a,b;
+
+Public:
+
+stu ()
+
+{
+
+Cout <<" Default Constructor";
+
+}
+
+};
+
+Int main ()
+
+{
+
+stu s();
+
+retum 0;
+
+}
